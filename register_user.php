4d9df188cbdd1129e5c3764304eefaf4414f111e
AM2211012957 <151082736+AM2211012957@users.noreply.github.com>
2023-11-17 04:19:14 +0800

    Add files via upload

---
+++ register_user.php
@@ -0,0 +1,95 @@
+<?php
+// Database connection (replace with your database credentials)
+$host = 'localhost';
+$dbname = 'lib_madani';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+// Registration logic
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash the password
+
+    try {
+        $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+        $stmt->execute([$username, $password]);
+        $registrationSuccess = true;
+    } catch (PDOException $e) {
+        $registrationError = "Registration failed: " . $e->getMessage();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <!-- Bootstrap -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+   <!-- Font Awesome -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+   <title>LIBRARY MADANI|USER</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #00ff5573;">
+      LIBRARY MADANI
+   </nav>
+
+<div class="container">
+      <div class="text-center mb-4">
+         <h3>WELCOME USER</h3>
+         <p class="text-muted">Register Here</p>
+      </div>
+
+<?php if (isset($registrationSuccess)): ?>
+    <p style="color: green;">Registration successful! You can now log in.</p>
+<?php endif; ?>
+
+<?php if (isset($registrationError)): ?>
+    <p style="color: red;"><?php echo $registrationError; ?></p>
+<?php endif; ?>
+
+<!-- Registration Form -->
+<div class="container d-flex justify-content-center">
+         <form action="" method="post" style="width:50vw; min-width:300px;">
+            <div class="row mb-3">
+               <div class="col">
+                  <label class="form-label">Username:</label>
+                  <input type="text" class="form-control" name="username" placeholder="Username" required>
+               </div>
+
+               <div class="col">
+                  <label class="form-label">Password:</label>
+                  <input type="password" class="form-control" name="password" placeholder="Password" required>
+               </div>
+            </div>
+
+            <div>
+               <button type="submit" class="btn btn-success" name="login">LOGIN</button><br>
+                <a href="login_user.php" class="btn btn-dark mb-3">Back</a>
+            </div>
+         </form>
+      </div>
+   </div>
+    
+   
+
+
+ <!-- Bootstrap -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</body>
+</html>
